3da1d2a926298ba1bfb4918cd730c0f86d1849c7
Somil Jain <somiljain2104@gmail.com>
2025-11-10 11:26:40 +0530

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/SHL/Frontend/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
